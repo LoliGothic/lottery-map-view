b0e402e1cc770c4c04b3ac192ba595b45b21c3e2
daisuke.miyoshi <nana40481023@gmail.com>
2022-11-16 22:32:06 +0900

    Fix ログイン画面の色の修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,40 @@
-import styles from "../styles/Home.module.css"
-import $ from 'jquery'
+import styles from "../styles/Home.module.css";
+import $ from "jquery";
 
 export default function Home() {
   const switching = () => {
-      $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-  }
+    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+  };
 
   return (
+    <div className={styles.body}>
     <div className={styles.loginPage}>
-  <div className={styles.form}>
-    <form className={styles.registerForm}>
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p className={styles.message}>Already registered? <a href="#" onClick={switching}>Sign In</a></p>
-    </form>
-    <form className={styles.loginForm}>
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <button>login</button>
-      <p className={styles.message}>Not registered? <a href="#" onClick={switching}>Create an account</a></p>
-    </form>
-  </div>
-</div>
-  )
+      <div className={styles.form}>
+        <form className={styles.registerForm}>
+          <input type="text" placeholder="name" required />
+          <input type="password" placeholder="password" required />
+          <input type="text" placeholder="email address" required />
+          <button>create</button>
+          <p className={styles.message}>
+            Already registered?{" "}
+            <a href="#" onClick={switching}>
+              Sign In
+            </a>
+          </p>
+        </form>
+        <form className={styles.loginForm}>
+          <input type="text" placeholder="username" required />
+          <input type="password" placeholder="password" required />
+          <button>login</button>
+          <p className={styles.message}>
+            Not registered?{" "}
+            <a href="#" onClick={switching}>
+              Create an account
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+    </div>
+  );
 }
